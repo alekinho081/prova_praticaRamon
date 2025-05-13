c71779789b0944a10457cbe27782c74fd8318e0a
Alekinho <alexborbajr0@gmail.com>
2025-05-13 17:04:42 -0300

    consertando função getLog que havia quebrado no último commit

--- server.js
+++ server.js
@@ -4,36 +4,36 @@ const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 
 function addLog(req, res) {
-    try{
+    try {
         const { nome } = req.body
         const id = uuidv4()
         const data = new Date()
         let mensagem = `${id} -- ${data.getFullYear()}-${data.getMonth()}-${data.getDay()}  ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()} -- ${nome}\n`
         fs.appendFileSync('./logs.txt', mensagem)
         res.status(201).send(`ID gerado: ${id}, mensagem adicionada ao arquivo de logs com sucesso`)
-    }catch(err){
+    } catch (err) {
         res.status(500).send(`Erro encontrado ${err}`)
     }
 }
 
 function getLog(req, res) {
-    try{
-        let id  = req.params
+    try {
+        let { id } = req.params
         const data = fs.readFileSync('./logs.txt').toString()
         const mensagem = data.split('\n')
         let logs = mensagem.map(e => e.split(' '))
         let mensagemLog
-    
+
         let uou = () => {
             for (let i = 0; i < logs.length; i++) {
                 if (logs[i].includes(id)) {
                     mensagemLog = logs[i].join(' ')
                     return true
                 } else {
-    
+
                     console.log('Nada encontrado ainda')
                 }
-    
+
             }
             return false
         }
@@ -42,7 +42,7 @@ function getLog(req, res) {
         } else {
             res.status(404).send('ID não encontrado')
         }
-    }catch(err){
+    } catch (err) {
         res.status(500).send(`Erro detectado ${err}`)
     }
 
@@ -56,5 +56,3 @@ app.get('/logs/:id', getLog)
 app.listen(8000, () => {
     console.log('servidor iniciado')
 })
-
-

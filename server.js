0b2b79f6ca4f3d2d310357d4fbcfbc10686012c2
Alekinho <alexborbajr0@gmail.com>
2025-05-13 16:56:44 -0300

    função de pegar log por id completa

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 
-function addLog (req, res) {
-    const {nome} = req.body
+function addLog(req, res) {
+    const { nome } = req.body
     const id = uuidv4()
     const d = new Date()
     let mensagem = `${id} -- ${d.getFullYear()}-${d.getMonth()}-${d.getDay()}  ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} -- ${nome}\n`
@@ -13,10 +13,31 @@ function addLog (req, res) {
 }
 
 function getLog(req, res) {
-    const id = req.param
+    let { id } = req.params
     const data = fs.readFileSync('./logs.txt').toString()
     const mensagem = data.split('\n')
-    
+    let logs = mensagem.map(e => e.split(' '))
+    let mensagemLog 
+
+    let uou = () => {
+        for (let i = 0; i < logs.length; i++) {
+            if (logs[i].includes(id)) {
+                mensagemLog = logs[i].join(' ')
+                return true
+            } else {
+                
+                console.log('Nada encontrado ainda')
+            }
+
+        }
+        return false
+    }
+    if (uou()) {
+        res.status(200).send(`Log encontrado: ${mensagemLog} `)
+    } else {
+        res.status(404).send('ID não encontrado')
+    }
+
 }
 
 app.use(express.json())
@@ -24,7 +45,7 @@ app.use(express.json())
 app.post('/logs', addLog)
 app.get('/logs/:id', getLog)
 
-app.listen(8000, ()=> {
+app.listen(8000, () => {
     console.log('servidor iniciado')
 })
 

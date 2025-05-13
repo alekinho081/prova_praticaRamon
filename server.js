8d99db81d663e8a8f46ded7f6b33012bfd720f56
Alekinho <alexborbajr0@gmail.com>
2025-05-13 15:49:54 -0300

    consertando função addLogs para mostrar a data de forma correta

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const { v4: uuidv4 } = require('uuid')
 function addLog (req, res) {
     const {nome} = req.body
     const id = uuidv4()
-    let mensagem = `${id} -- ${Date.now().toString()} -- ${nome}\n`
+    const d = new Date()
+    let mensagem = `${id} -- ${d.getFullYear()}-${d.getMonth()}-${d.getDay()}  ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} -- ${nome}\n`
     fs.appendFileSync('./logs.txt', mensagem)
     res.status(201).send(`ID gerado: ${id}, mensagem adicionada ao arquivo de logs com sucesso`)
 }
 
 
+
+
 app.use(express.json())
 
 app.post('/logs', addLog)
-
+app.get('/logs/:id', getLog)
 
 app.listen(8000, ()=> {
     console.log('servidor iniciado')
 })
+
+

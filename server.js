f14c7b26b7f5da997c3fceaeababe0b157f2964a
Alekinho <alexborbajr0@gmail.com>
2025-05-13 15:05:37 -0300

    rota para criar mensagem pronta, e função modificada para quebrar linha ao adicionar no txt

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-
+const { v4: uuidv4 } = require('uuid')
 
 function addLog (req, res) {
     const {nome} = req.body
     const id = uuidv4()
-    let mensagem = `${id} -- ${Date.now().toString()} -- ${nome}`
-    fs.appendFileSync('../logs.txt', mensagem)
+    let mensagem = `${id} -- ${Date.now().toString()} -- ${nome}\n`
+    fs.appendFileSync('./logs.txt', mensagem)
     res.status(201).send(`ID gerado: ${id}, mensagem adicionada ao arquivo de logs com sucesso`)
 }
+
+
+app.use(express.json())
+
+app.post('/logs', addLog)
+
 
 app.listen(8000, ()=> {
     console.log('servidor iniciado')
